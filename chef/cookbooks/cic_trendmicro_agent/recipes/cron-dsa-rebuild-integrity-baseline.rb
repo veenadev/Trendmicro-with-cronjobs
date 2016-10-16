7b75e14423cd978aee0a2e6516e6b0d64d58ec45
dev_veena <veenavdev@gmail.com>
2016-10-16 18:20:26 -0400

    Update and rename dsa-rebuild-integrity-baseline.rb to cron-dsa-rebuild-integrity-baseline.rb

---
+++ chef/cookbooks/cic_trendmicro_agent/recipes/cron-dsa-rebuild-integrity-baseline.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: cic_trendmicro_agent
+# Recipe:: Re-build an integrity baseline for the node
+#
+# Copyright 2015, Trend Micro
+#
+# License as per [repo](master/LICENSE)
+#
+# *********************************************************************
+# * Re-builds an integrity baseline for the current node
+# *********************************************************************
+cron 'dsa-rebuild-integrity-baseline' do
+  #action node.tags.include?('dsa-integrity-scan') ? :create : :delete
+  minute '0'
+  hour '0'
+  day '0'
+  month '0'
+  weekday '1'
+  user 'veena.dev'
+  mailto 'veena.dev@reancloud.com'
+  home '/home/'
+  command '/opt/ds_agent/dsa_control -m "RebuildBaseline:true"'
+
+  }.join(' ')
+end

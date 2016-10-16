646ffc045bb848ad35f7ab5a8ed3e01c566714f0
dev_veena <veenavdev@gmail.com>
2016-10-16 18:13:43 -0400

    Update dsa-integrity-scan.rb

--- chef/cookbooks/cic_trendmicro_agent/recipes/dsa-integrity-scan.rb
+++ chef/cookbooks/cic_trendmicro_agent/recipes/dsa-integrity-scan.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: cic_trendmicro_agent
-# Recipe:: Perform integrity scan
+# Recipe::dsa-integrity-scan.rb
 #
 # Copyright 2015, Trend Micro
 #
@@ -10,17 +10,18 @@
 # * Perform integrity scan
 # *********************************************************************
 
-dsa_args = "-m \"IntegrityScan:true\"" # create an integrity baseline and send it to the manager
 
-if node[:platform_family] =~ /win/
-	powershell_script 'prompt_ds_agent' do
-	  code <<-EOH
-		& $Env:ProgramFiles"\\Trend Micro\\Deep Security Agent\\dsa_control" #{dsa_args}
-	  EOH
-	end
-else
-	execute 'prompt_ds_agent' do
-		command "/opt/ds_agent/dsa_control #{dsa_args}"
-	end
+cron 'dsa-integrity-scan' do
+  #action node.tags.include?('dsa-integrity-scan') ? :create : :delete
+  minute '30'
+  hour '9'
+  day '*'
+  month '*'
+  week '*'
+  user 'veena.dev'
+  mailto 'veena.dev@reancloud.com'
+  home '/home/'
+  command '/opt/ds_agent/dsa_control -m "IntegrityScan:true"'
+
+  }.join(' ')
 end
-Chef::Log.info "The Deep Security agent will check in with the manager shortly"

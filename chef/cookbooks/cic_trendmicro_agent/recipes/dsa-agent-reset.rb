6f44a678af98060db2b2d756ac40cd7525628e63
dev_veena <veenavdev@gmail.com>
2016-10-16 17:53:20 -0400

    Update dsa-agent-reset.rb

--- chef/cookbooks/cic_trendmicro_agent/recipes/dsa-agent-reset.rb
+++ chef/cookbooks/cic_trendmicro_agent/recipes/dsa-agent-reset.rb
@@ -1,26 +1,20 @@
 #
 # Cookbook Name:: cic_trendmicro_agent
-# Recipe:: Resets agent 
-#
+# Recipe::dsa-agent-reset.rb
 # Copyright 2015, Trend Micro
-#
-# License as per [repo](master/LICENSE)
-#
 # *********************************************************************
 # *Resets Agent
 # *********************************************************************
 
-dsa_args = "-r" # force a heartbeat with the manager
-
-if node[:platform_family] =~ /win/
-	powershell_script 'prompt_ds_agent' do
-	  code <<-EOH
-		& $Env:ProgramFiles"\\Trend Micro\\Deep Security Agent\\dsa_control" #{dsa_args}
-	  EOH
-	end
-else
-	execute 'prompt_ds_agent' do
-		command "/opt/ds_agent/dsa_control #{dsa_args}"
-	end
+cron 'dsa-agent-reset' do
+  action node.tags.include?('dsa-agent-reset') ? :create : :delete
+  minute '0'
+  hour '0'
+  weekday '0'
+  user 'veena.dev'
+  mailto 'veena.dev@reancloud.com'
+  home '/home/'
+  command %W{
+   /opt/ds_agent/dsa_control -r
+  }.join(' ')
 end
-Chef::Log.info "The Deep Security agent will check in with the manager shortly"

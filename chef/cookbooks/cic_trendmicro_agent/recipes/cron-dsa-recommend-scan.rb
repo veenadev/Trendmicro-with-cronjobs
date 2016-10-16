d3c1fac42bd333528aee12eb0b55b24042ae2bc7
dev_veena <veenavdev@gmail.com>
2016-10-16 18:22:48 -0400

    Update and rename dsa-recommend-scan.rb to cron-dsa-recommend-scan.rb

---
+++ chef/cookbooks/cic_trendmicro_agent/recipes/cron-dsa-recommend-scan.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: cic_trendmicro_agent
+# Recipe:: dsa-recommend-scan.rb
+#
+# Copyright 2015, Trend Micro
+#
+# License as per [repo](master/LICENSE)
+#
+# *********************************************************************
+# * Recommend a security policy for the node
+# *********************************************************************
+cron 'dsa-recommend-scan' do
+  #action node.tags.include?('dsa-recommend-scan') ? :create : :delete
+  minute '0'
+  hour '0'
+  weekday '1'
+  user 'veena.dev'
+  mailto 'veena.dev@reancloud.com'
+  home '/home/'
+  command '/opt/ds_agent/dsa_control -m "RecommendationScan:true"'
+  }.join(' ')
+end
